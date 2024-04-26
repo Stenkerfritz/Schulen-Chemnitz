f358fa58ac00426005f4651c918d03a959f03cb0
Stenkerfritz <160107841+Stenkerfritz@users.noreply.github.com>
2024-04-26 07:23:13 +0200

    Anfang

---
+++ main.py
@@ -0,0 +1,15 @@
+import document
+import requests
+from bs4 import BeautifulSoup
+import json
+
+if __name__ == "__main__":
+    url = (f"https://portal-chemnitz.opendata.arcgis.com/datasets/4c331993dab54b49bbc9debfc5928ec3_0/explore?location=50.822923%2C12.887629%2C12.21&showTable=true")
+    response = requests.get(url)
+    data = response.json()
+
+    json_filename = "chemnitz_schulen.json"
+    with open(json_filename, 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
+    print(f"Schuldaten wurden geschrieben nach: {json_filename}")
